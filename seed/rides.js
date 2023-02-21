030c6737822ec9d3ff16bc6466b29135e980197d
Joshua Jumelet <joshuajumelet@gmail.com>
2023-02-21 12:33:16 -0800

    add seed data to rides.js

--- seed/rides.js
+++ seed/rides.js
@@ -7,9 +7,15 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 const main = async () => {
   const rides = [
     {
-      name: 'Six flags',
-      city: '',
-      logo: ''
+      name: 'Batman the Dark Knight',
+      image:
+        'https://i.pinimg.com/originals/c9/87/80/c987804a202a9d4de6a5e4d064973ee0.jpg',
+      description:
+        'The ride is based on the popular Batman franchise and provides riders with a thrilling experience through intense twists, turns, and drops.',
+      category: 'Roller Coaster',
+      height: '54 inches',
+      speed: '55mph',
+      park: ''
     }
   ]
 
