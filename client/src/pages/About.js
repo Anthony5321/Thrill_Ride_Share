34726430a87b4cdd14f1aa49beb3ab18c886d858
Joshua Jumelet <joshuajumelet@gmail.com>
2023-02-22 08:24:30 -0800

    create About.js file

---
+++ client/src/pages/About.js
@@ -0,0 +1,39 @@
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About Page</h1>
+      <span>
+        <p>
+          Welcome to our theme park and ride database! Our website is designed
+          to help users find information about theme parks and rides from all
+          around the world. Whether you're planning a family vacation or just
+          want to explore the world of amusement parks, our website has
+          everything you need to know.
+        </p>
+        <p>
+          At our website, users can add new theme parks and rides to our
+          database, making it a comprehensive resource for all things related to
+          amusement parks. Our goal is to provide users with accurate and
+          up-to-date information about every theme park and ride in the world,
+          so that they can make informed decisions about where to go and what
+          todo.
+        </p>
+        <p>
+          Our website is user-friendly and easy to navigate, with a search
+          function that allows users to filter their results based on location,
+          type of ride, and other important factors. Our team is dedicated to
+          providing the best possible experience for our users, and we are
+          always working to improve our website and add new features. We believe
+          that everyone should have the opportunity to enjoy the thrill of
+          amusement parks, and our website is designed to make that a reality.
+        </p>
+        <p>
+          Thank you for visiting our website, and we hope that you find the
+          information here helpful in planning your next theme park adventure!
+        </p>
+      </span>
+    </div>
+  )
+}
+
+export default About
