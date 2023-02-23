dc6213ae4f46c6eaaf23c85cefe5a251d9c603ca
Joshua Jumelet <joshuajumelet@gmail.com>
2023-02-23 08:52:46 -0800

    create newly added ride feature to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ import RideCard from '../components/RideCard'
 import ParkCard from '../components/ParkCard'
 import { Link } from 'react-router-dom'
 
-const Home = ({ park, parkName }) => {
+const Home = ({ park, ride }) => {
   const [searchResults, setSearchResults] = useState([])
   const [searched, toggleSearched] = useState(false)
   const [searchQuery, setSearchQuery] = useState('')
@@ -21,6 +21,8 @@ const Home = ({ park, parkName }) => {
   const handleChange = (event) => {
     setSearchQuery(event.target.value)
   }
+
+  const newly_add = ride[ride.length - 1]
 
   return (
     <div>
@@ -38,11 +40,7 @@ const Home = ({ park, parkName }) => {
           <section className="container-grid">
             {searchResults.map((result) => (
               <Link to={`/rides/details/${result._id}`} key={result._id}>
-                <RideCard
-                  name={result.name}
-                  image={result.image}
-                  // ride={parkName(result.park)}
-                />
+                <RideCard name={result.name} image={result.image} />
               </Link>
             ))}
           </section>
@@ -61,6 +59,16 @@ const Home = ({ park, parkName }) => {
             </Link>
           ))}
         </section>
+      </div>
+      <div>
+        <h2>Newly Added Ride</h2>
+        <Link
+          to={`/rides/details/${newly_add?._id}`}
+          key={newly_add?._id}
+          className="container-grid"
+        >
+          <RideCard name={newly_add?.name} image={newly_add?.image} />
+        </Link>
       </div>
     </div>
   )
